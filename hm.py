0f14c44fd4281c6938d878f94053277465a53e84
Sendo kame <sendo@sendokame.com>
2022-08-28 20:04:01 -0400

    Source code

--- hm.py
+++ hm.py
@@ -12,32 +12,32 @@ algorithms = {
     'sha512': hashlib.sha512
 }
 
-arguing.set('-h')
-arguing.set('-w')
-arguing.set('-a', default='md5')
+hashed = arguing.set('-h')
+wordlist = arguing.set('-w')
+algorithm = arguing.set('-a', default='md5')
 
 # Error
-if not os.path.exists(arguing.get('-w')):
+if not os.path.exists(wordlist):
     exit('- Wordlist does not exists.')
 
-if not arguing.get('-a') in hashlib.algorithms_available or arguing.check('-sa'):
+if algorithm not in hashlib.algorithms_available or arguing.check('-sa'):
     print('~ Supported algorithms:')
     exit(',\n'.join(algorithm for algorithm in list(algorithms)))
 
 # Main
-with open(arguing.get('-w')) as file:
+with open(wordlist) as file:
     file = file.read().strip().split('\n')
 
 start = timeit()
-algorithm = algorithms[arguing.get('-a')]
+algorithm = algorithms[algorithm]
 print(f'~ Starting search for "{arguing.get("-h")}" in {arguing.get("-a")}.')
 
 for password in file:
     hash = algorithm(bytes(password, 'utf-8')).hexdigest()
 
-    if hash == arguing.get('-h'):
+    if hash == hashed:
         print('\n+ The clear text hash is: ' + password)
-        print(f'+ Hash cracked on {round(timeit() - start)}.')
-        exit(1)
+        exit(f'+ Hash cracked on {round(timeit() - start)}.')
 
-print(f'\n- Sorry, there is no match for "{arguing.get("-h")}" on {arguing.get("-a")}.')
+print(
+    f'\n- There is no match for "{hashed}" on {algorithm.__qualname__}.')
